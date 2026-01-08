3cd76dfae671edd8d9f0ed59d3549c9301e8f724
isblu <mendislison0@gmail.com>
2026-01-09 02:20:26 +0530

    cleanup

--- anykernel.sh
+++ anykernel.sh
@@ -8,7 +8,7 @@ kernel.string=EOL-Kernel
 kernel.revision=4.14
 kernel.made=isblu @ LizzyGobbler
 anykernel3.made=osm0sis @ xda-developers
-kernel.compiler=Proton Clang 13.0.0
+kernel.compiler=Neutron Clang 19.0.0
 message.word=Thank you for installing EOL-Kernel!
 do.devicecheck=1
 do.cleanup=1
@@ -20,26 +20,16 @@ device.name3=
 supported.versions=14-16
 supported.patchlevels=
 supported.vendorpatchlevels=
-'; } # end properties
+'; }
 
-# BLOCK PARTITION PATH (This fixes your error)
 block=/dev/block/bootdevice/by-name/boot
 is_slot_device=0
 ramdisk_compression=auto
 patch_vbmeta_flag=auto
 
-# ---------------------
 
-# Import the backend installer logic
 . tools/ak3-core.sh
 
-# --- INSTALLATION ---
-
-# Boot Image Installation
 dump_boot;
 
-# (Optional) If you have dtb/dtbo, usually handled here, 
-# but dump_boot handles the main splitting.
-
 write_boot;
-# ---------------------
